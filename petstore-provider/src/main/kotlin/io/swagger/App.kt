6127b2e4b77b4623b67e2e241c3e02d8eaddfbea
Ronald Holshausen <uglyog@gmail.com>
2019-08-23 17:02:18 +1000

    chore: setup basic provider springboot app

---
+++ petstore-provider/src/main/kotlin/io/swagger/App.kt
@@ -0,0 +1,11 @@
+package io.swagger
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class App
+
+fun main(args: Array<String>) {
+  SpringApplication.run(App::class.java, *args)
+}
